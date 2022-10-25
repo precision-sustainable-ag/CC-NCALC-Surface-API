e28f395ed7b24867bf7f43fc32543767af207555
Rick Hitchcock <rickhitchcock@outlook.com>
2022-10-25 14:09:42 -0400

    fixed examples, redirect on error

--- public/index.js
+++ public/index.js
@@ -1,8 +1,7 @@
 window.addEventListener('load', () => {
-  if (location.origin.includes('localhost')) {
-    document.querySelectorAll('a').forEach(obj => {
-      obj.textContent = obj.textContent.replace('https://developapi.covercrop-ncalc.org', location.origin);
-      obj.href = obj.href.replace('https://developapi.covercrop-ncalc.org', location.origin);
-    });
-  }
+  console.log(location.origin);
+  document.querySelectorAll('a').forEach(obj => {
+    obj.textContent = obj.textContent.replace('https://api.covercrop-ncalc.org', location.origin);
+    obj.href = obj.href.replace('https://api.covercrop-ncalc.org', location.origin);
+  });
 });
